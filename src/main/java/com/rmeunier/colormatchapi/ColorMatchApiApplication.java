ef5949b8c02acd9423e602654a98a3aee8b3a926
Ramona Meunier <gramyy7@gmail.com>
2021-09-28 21:35:28 +0200

    spring project, adding docker compose

---
+++ src/main/java/com/rmeunier/colormatchapi/ColorMatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.rmeunier.colormatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorMatchApiApplication.class, args);
+	}
+
+}
