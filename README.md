e74062e6de4fc3136af2ad20c0ef513ac79bfc6a
Ramona Meunier <gramyy7@gmail.com>
2021-10-02 18:52:20 +0200

    working file import function, comments and notes

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Color Match API
 
 This is an API designed to match products of similar colors.
+
+## API
+The REST API has multiple endpoints for handling the products.
+
+### Product API endpoints
+1. `/importProducts` is a REST API endpoint for importing the products from a CSV file.
+It takes a file path String, in simple plain text format from the Response Body.
+
+### Tech
+I used Java 11 with Spring Boot for creating a REST API for this application.
+The application uses a PostgreSQL database as PostgreSQL is a great choice for dealing with CSV files.
+It also uses Docker to connect the server to the database in the same container, and for easier management.
+
--- src/main/java/com/rmeunier/colormatchapi/service/impl/ProductService.java
+++ src/main/java/com/rmeunier/colormatchapi/service/impl/ProductService.java
@@ -44,12 +44,18 @@ public class ProductService implements IProductService {
         }
 
         Product product = new Product(id, title, GenderId.valueOf(genderId), composition, sleeve, photo, url);
-
         saveProduct(product);
 
         return true;
     }
 
+    /**
+     * Import products from a CSV file of a given file path.
+     * As the processing of the file happens line by line using the Scanner,
+     * it does not store the whole file in the memory, therefore memory usage will be limited.
+     * @param filePath the path to the products CSV file.
+     */
+    //TODO batch processing
     @Override
     public void importProductsFromFilePath(String filePath) {
         loaderUtils = new FilePathLoader();
@@ -58,14 +64,16 @@ public class ProductService implements IProductService {
         try (FileInputStream inputStream = (FileInputStream) loaderUtils.loadFile(filePath);
              Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8)) {
 
-            // skipping first line for data type row
+            // skipping first line for header row
             if (sc.hasNextLine()) {
                 sc.nextLine();
             }
 
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                System.out.println(line);
+
+                Product product = parseStringIntoProduct(line);
+                saveProduct(product);
             }
 
             if (sc.ioException() != null) {
@@ -75,5 +83,33 @@ public class ProductService implements IProductService {
             System.err.println("Could not load file to import!");
             System.err.println(e.getMessage());
         }
+    }
+
+    /**
+     * Parses a single line of String from the file into a Product object.
+     * @param str a single line of the CSV file
+     * @return the Product object
+     */
+    // TODO potential error handling!!
+    private Product parseStringIntoProduct(String str) {
+        if (str.length() <= 0) {
+            return null;
+        }
+
+        String[] parsedString = str.split(",");
+
+        Product product = new Product();
+
+        if (parsedString.length > 0) {
+            product.setId(parsedString[0]);
+            product.setTitle(parsedString[1]);
+            product.setGenderId(GenderId.valueOf(parsedString[2]));
+            product.setComposition(parsedString[3]);
+            product.setSleeve(parsedString[4]);
+            product.setPath(parsedString[5]);
+            product.setUrl(parsedString[6]);
+        }
+
+        return product;
     }
 }
